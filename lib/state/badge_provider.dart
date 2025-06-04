fb2b8cdf1b5c635602d9a08e0fb0ef5afac8402d
nitsuj <nitsuj@gmail.com>
2025-06-04 09:32:28 -1000

    Update badge_provider.dart

--- lib/state/badge_provider.dart
+++ lib/state/badge_provider.dart
@@ -6,7 +6,23 @@ import 'package:hive_flutter/hive_flutter.dart';
 import '../models/badge.dart';
 
 class BadgeList extends StateNotifier<List<Badge>> {
-  BadgeList() : super(Hive.box<Badge>('badges').values.toList());
+  BadgeList() : super(_loadBadges());
+
+  /// Load all badges from Hive.
+  ///
+  /// If reading the box throws (e.g. corrupted data), the box is deleted,
+  /// reopened and an empty list is returned so the app can continue running.
+  static List<Badge> _loadBadges() {
+    try {
+      return Hive.box<Badge>('badges').values.toList();
+    } catch (_) {
+      Hive.deleteBoxFromDisk('badges');
+      // Reopen the box so future writes succeed. We intentionally
+      // ignore the returned Future here since the constructor is sync.
+      Hive.openBox<Badge>('badges');
+      return [];
+    }
+  }
 
   void add(Badge b) {
     Hive.box<Badge>('badges').add(b);
